91cae31d9c08a6a7ec5ac8b3d076f273c7112f34
James Goppert <james.goppert@gmail.com>
2023-01-26 09:33:59 -0500

    Switch docker to build image.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- docker/install/user_setup.sh
+++ docker/install/user_setup.sh
@@ -10,8 +10,11 @@ mkdir ~/.vnc && echo "$VNCPASSWD" | /opt/TurboVNC/bin/vncpasswd -f > ~/.vnc/pass
   openssl req -x509 -nodes -newkey rsa:3702 -keyout ~/.vnc/x509_private.pem -out ~/.vnc/x509_cert.pem -days 3650 -subj '/CN=www.mydom.com/O=My Company Name LTD./C=US'
 
 # setup .profile, note bashrc doesn't get sourced by docker by defualt, .profile does
-echo "source /opt/ros/humble/setup.bash" >> ~/.profile
-echo "source /opt/ws_ros_gz/install/setup.sh" >> ~/.profile
-echo "export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp" >> ~/.profile
-echo "export GZ_SIM_RESOURCE_PATH=/workdir/simulation/models:/workdir/simulation/worlds" >> ~/.profile
-
+cat << EOF >> ~/.profile
+source /opt/ros/humble/setup.bash
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+export GZ_SIM_RESOURCE_PATH=/workdir/simulation/models:/workdir/simulation/worlds
+if [ -f /workdir/gazebo/install/setup.bash ]; then
+  source /workdir/gazebo/install/setup.bash
+fi
+EOF
---
+++ scripts/build_gazebo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+set -x
+sudo apt update
+sudo apt-get install -y python3-vcstool python3-colcon-common-extensions
+mkdir -p /workdir/gazebo/src
+cd /workdir/gazebo/src
+wget https://raw.githubusercontent.com/rudislabs/gazebodistro/cognipilot/collection-garden.yaml -O collection-garden.yaml
+vcs import < collection-garden.yaml
+
+git clone -b ros2 https://github.com/gazebosim/ros_gz.git
+
+sudo apt -y install \
+  $(sort -u $(find . -iname 'packages-'`lsb_release -cs`'.apt' -o -iname 'packages.apt' | grep -v '/\.git/') | sed '/gz\|sdf/d' | tr '\n' ' ')
+
+cd /workdir/gazebo
+colcon graph
+colcon build --cmake-args -DBUILD_TESTING=OFF --merge-install
