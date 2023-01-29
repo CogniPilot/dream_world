be06c64925ef19eff4b93de5dd98fb11a5a205cb
James Goppert <james.goppert@gmail.com>
2023-01-29 02:53:19 -0500

    Add follow script and update build script.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- scripts/build_gazebo.sh
+++ scripts/build_gazebo.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 set -e
 set -x
-sudo apt update
-sudo apt-get install -y python3-vcstool python3-colcon-common-extensions
-mkdir -p /workdir/gazebo/src
-cd /workdir/gazebo/src
-wget https://raw.githubusercontent.com/rudislabs/gazebodistro/cognipilot/collection-garden.yaml -O collection-garden.yaml
-vcs import < collection-garden.yaml
 
-#sudo apt -y install \
-#  $(sort -u $(find . -iname 'packages-'`lsb_release -cs`'.apt' -o -iname 'packages.apt' | grep -v '/\.git/') | sed '/gz\|sdf/d' | tr '\n' ' ')
+cd src
+sudo apt -y install \
+	$(sort -u $(find . -iname 'packages-'`lsb_release -cs`'.apt' -o -iname 'packages.apt' | grep -v '/\.git/') | sed '/gz\|sdf/d' | tr '\n' ' ')
 
-cd /workdir/gazebo
-colcon graph
+cd ..
 colcon build --cmake-args -DBUILD_TESTING=OFF --merge-install
