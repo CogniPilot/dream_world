30263116d89a5b7698202e1bc713f3475f7d2927
James Goppert <james.goppert@gmail.com>
2023-01-29 23:17:09 -0500

    Update odometry plugin for rover, remove joy.sh
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- scripts/joy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-ros2 run joy joy_node &
-sleep 2
-ros2 run ros_gz_bridge parameter_bridge /joy@sensor_msgs/msg/Joy@gz.msgs.Joy &
-
-ros2 run ros_gz_bridge parameter_bridge /poly@synapse_msgs/msg/PolynomialTrajectory@gz.msgs.PolynomialTrajectory &
-
-ros2 run ros_gz_bridge parameter_bridge /model/MR_Buggy3/odometry@nav_msgs/msg/Odometry@gz.msgs.Odometry &
