cca83ee132318b311e6436002b1ce9365eab336f
James Goppert <james.goppert@gmail.com>
2023-02-09 16:01:42 -0500

    Revert changes to rover launch file.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- launch/rover.launch.py
+++ launch/rover.launch.py
@@ -8,54 +8,27 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-
-    # parameters
-    logger = LaunchConfiguration("log_level")
-    model = LaunchConfiguration('model')
-    sitl_binary = LaunchConfiguration('sitl_binary')
-    use_sim_time = LaunchConfiguration('use_sim_time')
-
-    # variables
+    use_sim_time = True
+    logger = launch.substitutions.LaunchConfiguration("log_level")
     gui_config = get_package_share_directory('dream') + '/config/gui.config'
-    world = f'{model}_map.sdf'
-
     return LaunchDescription([
         launch.actions.DeclareLaunchArgument(
             "log_level",
             default_value=["warn"],
             description="Logging level"),
-        launch.actions.DeclareLaunchArgument(
-            "model",
-            default_value=["mrb3s"],
-            description="model name"),
-        launch.actions.DeclareLaunchArgument(
-            "sitl_binary",
-            default_value=["cerebri"],
-            description="SITL binary"),
-        launch.actions.DeclareLaunchArgument(
-            "use_sim_time",
-            default_value=True,
-            description="use sim time"),
         ExecuteProcess(
-            cmd=f"terminator -u -T cerebri --geometry=500x700+0+0 -e {sitl_binary} 2>&1".split(),
+            cmd="terminator -u -T cerebri --geometry=500x700+0+0 -e cerebri 2>&1".split(),
             output="log",
             shell=True
         ),
         IncludeLaunchDescription(PythonLaunchDescriptionSource(
-            get_package_share_directory('corti') +  '/launch/corti.launch.py'),
-            launch_arguments={
-                'model': model,
-                'use_sim_time': use_sim_time,
-            }.items()),
+            get_package_share_directory('corti') +  '/launch/corti.launch.py')),
         IncludeLaunchDescription(PythonLaunchDescriptionSource(
-            get_package_share_directory('electrode') + '/launch/electrode.launch.py'),
-            launch_arguments={
-                'use_sim_time': use_sim_time,
-            }.items()),
+            get_package_share_directory('electrode') + '/launch/electrode.launch.py')),
         IncludeLaunchDescription(PythonLaunchDescriptionSource(
             get_package_share_directory('ros_gz_sim') + '/launch/gz_sim.launch.py'),
             launch_arguments={
-                'gz_args': f'{world} -v 0 --gui-config {gui_config} -r'
+                'gz_args': f'mrb3s_map.sdf -v 0 --gui-config {gui_config} -r'
             }.items()),
         Node(
            package='ros_gz_bridge',
@@ -69,10 +42,10 @@ def generate_launch_description():
              # trajectory from ROS to Gazesbo
              '/traj@synapse_msgs/msg/BezierTrajectory@gz.msgs.BezierTrajectory',
              # odometry from Gazebo model to ROS
-             f'/model/{model_name}/odometry@nav_msgs/msg/Odometry@gz.msgs.Odometry',
-             f'/model/{model_name}/odometry_with_covariance@nav_msgs/msg/Odometry@gz.msgs.OdometryWithCovariance',
+             '/model/mrb3s/odometry@nav_msgs/msg/Odometry@gz.msgs.Odometry',
+             '/model/mrb3s/odometry_with_covariance@nav_msgs/msg/Odometry@gz.msgs.OdometryWithCovariance',
              # Sensors
-             '/world/default/model/{model_name}/link/RPLIDAR_A1M8/Base/sensor/lidar/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan',
+             '/world/default/model/mrb3s/link/RPLIDAR_A1M8/Base/sensor/lidar/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan',
              # ros args
              '--ros-args', '--log-level', logger,
            ],
